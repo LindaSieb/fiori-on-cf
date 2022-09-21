374f31ebaa6098863b7f4582604942d1d33f3eec
Linda Siebert <linda.siebert@sap.com>
2022-09-21 18:12:53 +0200

    Add url append

--- test/specs/checkTitle.js
+++ test/specs/checkTitle.js
@@ -1,6 +1,6 @@
 describe('Test Application', () => {
     it('should return correct title on the page', async () => {
-        await browser.url(`/`)
+        await browser.url(`/2f0f75e4-e675-4cb2-8050-b7e06af7f94b.webdriveriotesting.webdriveriotesting-0.0.1/index.html`)
         await expect($('#container-webdriveriotesting---WebDriverIOTesting--page-title-inner')).toHaveTextContaining('Testing with WebDriverIO');
     });
 });
